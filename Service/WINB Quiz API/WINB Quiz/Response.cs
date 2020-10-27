04d694f42a97357dc5dbee429046a501bcc9e35c
JosephSneha <73291783+JosephSneha@users.noreply.github.com>
2020-10-27 09:42:01 +0530

    api

---
+++ Service/WINB Quiz API/WINB Quiz/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WINB_Quiz
+{
+    public class Response
+    {
+        public TimeSpan Time { get; set; }
+        public string[] Answers { get; set; }
+    }
+}
