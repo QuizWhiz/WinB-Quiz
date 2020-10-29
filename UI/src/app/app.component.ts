bbecbced26bd89f62b44ab9410688aaabed4504b
JosephSneha <73291783+JosephSneha@users.noreply.github.com>
2020-10-29 11:08:03 +0530

    Image and input control

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -5,37 +5,43 @@ import {​ DataService }​ from './service/data.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 }​)
-export class AppComponent implements OnInit{​
+export class AppComponent implements OnInit {​
     title = 'WinBQuiz';
     public valArray: string[] = [];
     public isSubmitted = false;
+    public imageAvailable = false;
     mockQuestions = [
         {​
             option: 1,
             ques: "1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ornare ex, maximus porta enim scelerisque in. Cras sit amet turpis ut est molestie maximus.?",
-            options: ['one', 'two', 'three']
+            options: ['one', 'two', 'three'],
+            image: '../assets/winbLogo.png'
         }​,
         {​
             option: 2,
             ques: "2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ornare ex, maximus porta enim scelerisque in. Cras sit amet turpis ut est molestie maximus.?",
-            options: ['one', 'two', 'three']
+            options: ['one', 'two', 'three'],
+            image: null
         }​,
         {​
             option: 3,
             ques: "3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ornare ex, maximus porta enim scelerisque in. Cras sit amet turpis ut est molestie maximus.?",
-            options: ['one', 'two', 'three']
+            options: ['one', 'two', 'three'],
+            image: '../assets/winbLogo.png'
         }​,
         {​
             option: 4,
             ques: "4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ornare ex, maximus porta enim scelerisque in. Cras sit amet turpis ut est molestie maximus.?",
-            options: ['one', 'two', 'three']
+            options: ['one', 'two', 'three'],
+            image: null
         }​,
         {​
             option: 5,
             ques: "5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ornare ex, maximus porta enim scelerisque in. Cras sit amet turpis ut est molestie maximus.?",
-            options: ['one', 'two', 'three']
+            options: ['one', 'two', 'three'],
+            image: '../assets/winbLogo.png'
         }​
-    ]
+    ];
     constructor(private dataService: DataService) {​}​
     ngOnInit() {​
         this.dataService.getData().subscribe(response => {​
@@ -45,15 +51,15 @@ export class AppComponent implements OnInit{​
     submit() {​
         this.isSubmitted = true;
         this.dataService.postData(this.valArray).subscribe((result) => {​
-            console.log('saved:',result);
+            console.log('saved:', result);
         }​);
     }​
-    onSelected(e: Event, questionNumber: number){​
-        this.valArray[questionNumber-1] = (e.target as HTMLInputElement).value;
+    onSelected(e: Event, questionNumber: number) {​
+        this.valArray[questionNumber - 1] = (e.target as HTMLInputElement).value;
     }​
+
+    onInputChange(e: Event, questionNumber: number) {
+        this.valArray[questionNumber - 1] = (e.target as HTMLInputElement).value;
+    }
 }​
-    
-    
-  
-  
 
